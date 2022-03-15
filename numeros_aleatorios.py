0f75fce951e6763514d4144c71c3bfb3da580d02
Pedro Muñoz <pmz94@hotmail.com>
2022-03-15 15:55:24 -0700

    agregando cuartiles y percentiles

--- numeros_aleatorios.py
+++ numeros_aleatorios.py
@@ -4,8 +4,8 @@ import funciones
 
 tiempos = []
 for i in range(0, 853):
-    x = round(rm.uniform(2.1, 2.2), 2)
-    tiempos.append(x)
+	x = round(rm.uniform(2.1, 2.2), 2)
+	tiempos.append(x)
 
 n = len(tiempos)
 x = np.mean(tiempos)
