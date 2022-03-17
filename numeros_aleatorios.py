370e9315306b42aafb344446e7b7bc84bec0960c
Pedro Muñoz <pmz94@hotmail.com>
2022-03-17 15:54:07 -0700

    agregando graficas de probabilidad

--- numeros_aleatorios.py
+++ numeros_aleatorios.py
@@ -1,21 +1,40 @@
+import math
 import random as rm
 import numpy as np
-import funciones
+from prettytable import PrettyTable
 
-tiempos = []
-for i in range(0, 853):
-	x = round(rm.uniform(2.1, 2.2), 2)
-	tiempos.append(x)
+def rm_uniforme(a, b, r = rm.random()):
+	return round(a + r * (b - a), 2)
 
-n = len(tiempos)
-x = np.mean(tiempos)
-dv = np.std(tiempos)
-minimo = min(tiempos)
-maximo = max(tiempos)
+def rm_bernoulli(p, r = rm.random()):
+	return 1 if r >= p else 0
 
-print('n:', n)
-print('x:', x)
-print('dv:', dv)
-print('min:', minimo)
-print('max:', maximo)
-print(funciones.freq_table2(tiempos))
+def rm_normal(m, d):
+	return round(m + d * (sum([rm.random() for _ in range(12)]) - 6), 2)
+
+def rm_exponencial(uno_sobre_lambda, r = rm.random()):
+	return round(-uno_sobre_lambda * math.log(1 - r), 2)
+
+def rm_weibull(d, b, r = rm.random()):
+	return round(1 / d * (-math.log(1 - r)) ** (1 / b), 2)
+
+tabla = PrettyTable(field_names = ['uniforme', 'bernoulli', 'normal', 'exponencial', 'weibull'])
+
+for i in range(50):
+	r = rm.random()
+	tabla.add_row([
+		rm_uniforme(10, 100, r),
+		rm_bernoulli(0.6, r),
+		rm_normal(0, 1),
+		rm_exponencial(6, r),
+		rm_weibull(1, 5, r)
+	])
+
+print(tabla.get_string())
+
+def estadistica_descriptiva(x: list):
+	n = len(x)
+	media = np.mean(x)
+	dv = np.std(x)
+	minimo = min(x)
+	maximo = max(x)
