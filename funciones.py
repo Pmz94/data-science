bfbebdc7bfbdde4373274fac29fd05b5ea974cb0
Pedro Muñoz <pmz94@hotmail.com>
2022-04-18 15:53:57 -0700

    .

--- funciones.py
+++ funciones.py
@@ -7,6 +7,8 @@ import numpy as np
 import prettytable as pt
 import scipy.stats as st
 import unicodedata
+import datetime as dt
+import re
 
 
 def quitar_acentos(s):
@@ -124,9 +126,12 @@ def histograma(
 		plt.plot(kde_xs, kde.pdf(kde_xs), label = 'Tendencia')
 		plt.legend(loc = 'upper left')
 
-	if titulo: plt.title(titulo)
-	if label_x: plt.xlabel(label_x)
-	if label_y: plt.ylabel(label_y)
+	if titulo:
+		plt.title(titulo)
+	if label_x:
+		plt.xlabel(label_x)
+	if label_y:
+		plt.ylabel(label_y)
 	plt.show()
 
 
@@ -152,7 +157,8 @@ def grafica_serie_tiempo(
 	ejex_enteros = False
 ) -> None:
 	plt.plot(ejex, ejey, 'o-')
-	if ejex_enteros: plt.xticks(range(math.floor(min(ejex)), math.ceil(max(ejex)) + 1))
+	if ejex_enteros:
+		plt.xticks(range(math.floor(min(ejex)), math.ceil(max(ejex)) + 1))
 	plt.title(titulo)
 	plt.xlabel(etiquetax)
 	plt.ylabel(etiquetay)
@@ -176,3 +182,101 @@ def crappyhist(data: list, bins = 0, width = 140):
 			)
 		)
 	print('{:12.5f} |'.format(b[bins]))
+
+
+# Obtener complexion fisica segun IMC
+def complexion_fisica(altura, peso):
+	# peso en kg y altura en m
+	imc = peso / (altura ** 2)
+	# OJO el imc no distigue sexo, edad, grasa o musculo
+	# Digamos que esto define el volumen de la persona
+	complexion = ''
+	if -np.inf <= imc < 25:
+		complexion = 'Chico'
+	elif 25 <= imc < 35:
+		complexion = 'Medio'
+	elif 35 <= imc < np.inf:
+		complexion = 'Grande'
+	return complexion
+
+
+# Calcular CURP
+def sacar_curp(nombre: str, apellido_paterno: str, apellido_materno: str, sexo: str, fecha_nac: Union[str, dt.datetime], estado_nac: str) -> str:
+	nombre = quitar_acentos(nombre).upper().replace('Ñ', 'X')
+	apellido_paterno = quitar_acentos(apellido_paterno).upper().replace('Ñ', 'X')
+	apellido_materno = quitar_acentos(apellido_materno).upper().replace('Ñ', 'X')
+	estado_nac = quitar_acentos(estado_nac.replace(' ', '_').lower())
+	if isinstance(fecha_nac, str):
+		fecha_nac = dt.datetime.strptime(fecha_nac, '%Y-%m-%d')
+	# Primera letra y vocal del primer apellido
+	curp = apellido_paterno[0:2].upper()
+	# Primera letra del segundo apellido
+	curp += apellido_materno[0:1].upper()
+	# Primera letra del nombre de pila
+	curp += nombre[0:1].upper()
+	# Fecha de nacimiento (2 últimos dígitos del año, 2 del mes y 2 del día de nacimiento)
+	curp += fecha_nac.strftime('%y%m%d')
+	# Letra del sexo (H o M)
+	curp += sexo[0:1].upper()
+	# Dos letras correspondientes a la entidad de nacimiento segun RENAPO (Sonora => SR)
+	# en el caso de extranjeros, se marca como NE (Nacido Extranjero)
+	clave_estados = {
+		'aguascalientes': 'AS', 'baja_california': 'BC', 'baja_california_sur': 'BS',
+		'campeche': 'CC', 'coahuila': 'CL', 'colima': 'CM', 'chiapas': 'CS', 'chihuahua': 'CH',
+		'distrito_federal': 'DF', 'ciudad_de_mexico': 'DF', 'durango': 'DG', 'guanajuato': 'GT',
+		'guerrero': 'GR', 'hidalgo': 'HG', 'jalisco': 'JC', 'mexico': 'MC', 'estado_de_mexico': 'MC',
+		'michoacan': 'MN', 'morelos': 'MS', 'nayarit': 'NT', 'nuevo_leon': 'NL', 'oaxaca': 'OC',
+		'puebla': 'PL', 'queretaro': 'QT', 'quintana_roo': 'QR', 'san_luis_potosi': 'SP', 'sinaloa': 'SL',
+		'sonora': 'SR', 'tabasco': 'TC', 'tamaulipas': 'TS', 'tlaxcala': 'TL', 'veracruz': 'VZ',
+		'yucatan': 'YN', 'zacatecas': 'ZS'
+	}
+	if estado_nac in clave_estados:
+		curp += clave_estados[estado_nac]
+	else:
+		curp += 'NE'
+	# Primera consonante interna del primer apellido
+	curp += re.sub(f'[aeiou]', '', apellido_paterno)[1:2].upper()
+	# Primera consonante interna del segundo apellido
+	curp += re.sub(f'[aeiou]', '', apellido_materno)[1:2].upper()
+	# Primera consonante interna del nombre
+	curp += re.sub(f'[aeiou]', '', nombre)[1:2].upper()
+	# Dígito verificador del 0-9 para fechas de nacimiento hasta el año 1999 y A-Z para fechas de nacimiento a partir del 2000
+	int_char = [rm.randint(0, 9), chr(rm.randint(65, 90))]
+	curp += str(int_char[0] if fecha_nac.year <= 1999 else int_char[1])
+	# Homoclave, para evitar duplicaciones
+	curp += str(rm.randint(0, 9))
+	return curp
+
+
+# Obtener tu signo segun tu cumpleaños
+def sacar_signo(fecha_nac: Union[str, dt.datetime]):
+	if isinstance(fecha_nac, str):
+		fecha_nac = dt.datetime.strptime(fecha_nac, '%Y-%m-%d')
+	dia = int(fecha_nac.strftime('%d'))
+	mes = int(fecha_nac.strftime('%m'))
+	astro_sign = ''
+	if mes == 12:
+		astro_sign = 'Sagitario' if dia < 22 else 'Capricornio'
+	elif mes == 1:
+		astro_sign = 'Capricornio' if dia < 20 else 'Aquario'
+	elif mes == 2:
+		astro_sign = 'Aquario' if dia < 19 else 'Piscis'
+	elif mes == 3:
+		astro_sign = 'Piscis' if dia < 21 else 'Aries'
+	elif mes == 4:
+		astro_sign = 'Aries' if dia < 20 else 'Tauro'
+	elif mes == 5:
+		astro_sign = 'Tauro' if dia < 21 else 'Geminis'
+	elif mes == 6:
+		astro_sign = 'Geminis' if dia < 21 else 'Cancer'
+	elif mes == 7:
+		astro_sign = 'Cancer' if dia < 23 else 'Leo'
+	elif mes == 8:
+		astro_sign = 'Leo' if dia < 23 else 'Virgo'
+	elif mes == 9:
+		astro_sign = 'Virgo' if dia < 23 else 'Libra'
+	elif mes == 10:
+		astro_sign = 'Libra' if dia < 23 else 'Escorpio'
+	elif mes == 11:
+		astro_sign = 'Escorpio' if dia < 22 else 'Sagitario'
+	return astro_sign
